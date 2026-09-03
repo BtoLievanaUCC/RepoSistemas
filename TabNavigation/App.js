d8548a1fe51288eca80f618ddfbef08a2b078a72
Pedrito <pedrito@ucc.mx>
2026-09-02 20:58:48 -0600

    TabNavigation

---
+++ TabNavigation/App.js
@@ -0,0 +1,47 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import HomeScreen from './src/screens/HomeScreen';
+import ProfileScreen from './src/screens/ProfileScreen';
+import SearchScreen from './src/screens/SearchScreen';
+
+
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ 
+          headerShown: false,
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'gray',
+          }}
+          >
+        <Tab.Screen 
+        name="Inicio" 
+        component={HomeScreen} 
+        />
+        <Tab.Screen 
+        name="Buscar" 
+        component={SearchScreen} 
+        />
+        <Tab.Screen 
+        name="Perfil" 
+        component={ProfileScreen} 
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
